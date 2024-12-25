684479ffa477569f9c2d8cf2462b44e2d75609cf
furkanBilici <fbilici258@gmail.com>
2024-12-25 20:20:03 +0300

    tss eklemesi

--- Singleton/Class/CustomerManager.cs
+++ Singleton/Class/CustomerManager.cs
@@ -9,15 +9,25 @@ namespace Singleton.Class
     internal class CustomerManager
     {
         private static CustomerManager _customerManager;
+        static object _lockObject=new object();
+        
 
         private CustomerManager() { }
-        public static CustomerManager CreateAsSingleton ()
+        public static CustomerManager CreateAsSingleton ()//bu fonksiyon sayesinde nesne sadece bir kez üretilmiş oluyor ve bir nesne üzerinden çalışılmış oluyor
         {
-            if (_customerManager == null)
+            lock (_lockObject)// thread safe singleton için
             {
-                _customerManager= new CustomerManager();    
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
             }
             return _customerManager;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
         }
     }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using Singleton.Class;
+
+var customerManager = CustomerManager.CreateAsSingleton();//Açtığımız değişken bu fonksiyon sayesinde static ve bir nesne üzerinden çalışmış oluyor.
+customerManager.Save();
