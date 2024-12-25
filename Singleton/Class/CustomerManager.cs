2e32d2009af933af9b29b4209b86291d77f23b14
furkanBilici <fbilici258@gmail.com>
2024-12-25 19:59:35 +0300

    Customer Class

---
+++ Singleton/Class/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Class
+{
+    internal class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+
+        private CustomerManager() { }
+        public static CustomerManager CreateAsSingleton ()
+        {
+            if (_customerManager == null)
+            {
+                _customerManager= new CustomerManager();    
+            }
+            return _customerManager;
+        }
+    }
+}
